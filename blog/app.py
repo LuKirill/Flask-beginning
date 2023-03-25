19374d889ec543e92f2d250d07598e61a0a00a14
Kirill <kirill-mihalich@mail.ru>
2023-03-25 16:46:48 +0300

    added methods GET & POST

---
+++ blog/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.rute('/status/', methods=["GET", "POST"])
+def custom_status_code():
+    if request.method == "GET":
+        return "Response GET method"
+    return "Response POST method"
